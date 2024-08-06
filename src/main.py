9e58bcd2ec11e9a09fe330a85fa401f6a93ff2e0
Vitor Manoel <vitorearthur15@hotmail.com>
2024-08-06 01:05:48 -0300

    correção de erros

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ def training():
 
         for i in range(4):
             print(f"Reinicializando modelo: iteração {i+1}")
-            modelgreedy = hmm.GaussianHMM(n_components=5, init_params='st', covariance_type="full", n_iter = 100)
+            modelgreedy = hmm.GMMHMM(n_components=5, n_mix=3, init_params='st', covariance_type="full", n_iter = 100)
             modelgreedy.fit(mfcc_vector, lengths)
             if(modelgreedy.score(mfcc_vector, lengths) > model.score(mfcc_vector, lengths)): 
                 model = modelgreedy
